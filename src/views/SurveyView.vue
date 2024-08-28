14cc35f7d3417d18b6b4afedbd409309899ebc4c
crazyfullstack <mattsonjohnny2@gmail.com>
2024-08-28 09:38:49 -0400

    add comments for the questionindices

--- src/views/SurveyView.vue
+++ src/views/SurveyView.vue
@@ -30,7 +30,6 @@ export default {
         title: 'Which hobbies do you enjoy?',
         type: 'checkbox',
         meta: {
-          label: 'Select your hobbies',
           options: ['Reading', 'Traveling', 'Cooking', 'Sports']
         }
       },
@@ -41,16 +40,23 @@ export default {
         meta: { placeholder: 'Write something about yourself...' }
       }
     ]
-
-    const questionIndices = [0, 3, 5] // Indices for the questions to display
+    const questionIndices = [0, 2, 5]
+    // Indices for the questions to display
+    // For example, if `questionIndices = [0, 1, 4, 5]`,
+    // the survey will show 5 questions of the `questions` in pages.
+    // The first page will have the question at index 0,
+    // the second page will show the questions from index 1 to 3,
+    // and the last page will show the question at index 4.
     const currentPageIdx = ref(0)
 
     const currentSurvey = computed(() => {
       const startIdx =
         currentPageIdx.value === questionIndices.length - 1
-          ? 0
+          ? 0 // For review page
           : questionIndices[currentPageIdx.value]
-      const endIdx = questionIndices[currentPageIdx.value + 1] || questions.length
+
+      const endIdx =
+        questionIndices[currentPageIdx.value + 1] || questionIndices[questionIndices.length - 1]
       return {
         process: currentPageIdx.value + 1,
         total: questionIndices.length - 1,
